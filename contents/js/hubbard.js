56757e1944f535b2e431f987c9cfe6e8e6f26684
Pete Gadomski <pete.gadomski@gmail.com>
2014-06-23 09:55:17 -0600

    Load data with jsonp

--- contents/js/hubbard.js
+++ contents/js/hubbard.js
@@ -1,34 +1,38 @@
+var urlbase = "http://nae-rrs2.usace.army.mil:7777/pls/cwmsweb/jsonapi.timeseriesdata?"
+
+// TODO development vs. production switch?
+var timeseries = [
+{
+    "name": "Terminus range",
+//    "url": urlbase + "ts_codes=18844021,18845021,18846021,18847021,18848021,18849021,18850021,18851021,18852021,18853021,18854021,18855021,18856021,18857021,18858021,18859021,18860021,18861021,18862021,18863021,18864021,18865021,18866021,18867021,18868021,18869021,18870021,18871021,18872021,18873021&summary_interval=daily&floor=320&jsonp={callback}"
+    "url": "../../data/hubbard-avgrange-daily.js?callback=d3.jsonp.ntsfvAwWtPjZJKz"
+},
+{
+    "name": "Air temperature",
+    // "url": urlbase + "ts_codes=13260021&jsonp={callback}"
+    "url": "../../data/hubbard-avgtemp-daily.js?callback=d3.jsonp.temperature"
+}
+];
+
+var loadedData = [];
+
 var margin = {top: 10, right: 10, bottom: 100, left: 40},
     margin2 = {top: 430, right: 10, bottom: 20, left: 40},
-    width = 960 - margin.left - margin.right,
+    width = 945 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
-    height2 = 500 - margin2.top - margin2.bottom,
-    days = 24 * 60 * 60 * 1000;
+    height2 = 500 - margin2.top - margin2.bottom;
 
 var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S").parse;
 
 var xscale = d3.time.scale().range([0, width]),
-    x2scale = d3.time.scale().range([0, width]),
-    yscale = d3.scale.linear().range([height, 0]),
-    y2scale = d3.scale.linear().range([height2, 0]);
+    x2scale = d3.time.scale().range([0, width]);
 
 var xAxis = d3.svg.axis().scale(xscale).orient("bottom"),
     xAxis2 = d3.svg.axis().scale(x2scale).orient("bottom"),
-    yAxis = d3.svg.axis().scale(yscale).orient("left");
+    yAxis = d3.svg.axis().orient("left");
 
 var brush = d3.svg.brush()
-    .x(x2scale)
-    .on("brush", brushed);
-
-var line = d3.svg.line()
-    .defined(function(d) { return d.value; })
-    .x(function(d) { return xscale(d.date_time); })
-    .y(function(d) { return yscale(d.value); });
-
-var line2 = d3.svg.line()
-    .defined(function(d) { return d.value; })
-    .x(function(d) { return x2scale(d.date_time); })
-    .y(function(d) { return y2scale(d.value); });
+    .x(x2scale);
 
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -48,40 +52,19 @@ var context = svg.append("g")
     .attr("class", "context")
     .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-var data = null;
 
-d3.json("../../data/hubbard-avgrange-daily.json", function(error, d) {
-    data = d.map(function(d) {
-        return {
-            "date_time": parseDate(d.date_time),
-            "value": d.value
-        }
-    });
-    xscale.domain(d3.extent(data.map(function(d) { return d.date_time; })));
-    yscale.domain([320, d3.max(data.map(function(d) { return d.value; }))]);
-    x2scale.domain(xscale.domain());
-    y2scale.domain(yscale.domain());
+function draw() {
+    var defaultExtent = [d3.time.month.offset(new Date(), -2), new Date()];
+    
+    updateXscale();
+    yAxis.scale(visibleData()[0].yscale);
 
-    data = xscale.ticks(d3.time.day, 1).reduce(function(previous, current) {
-        d = {date_time: current};
-        if (data[0].date_time <= current)
-            d.value = data.shift().value;
-        else
-            d.value = null;
-        return previous.concat(d);
-    }, []);
-
-    focus.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line);
-
-    focus.selectAll("circle.datapoint").data(data, date_time)
+    focus.selectAll("path")
+            .data(visibleData())
         .enter()
-            .append("circle")
-            .attr("class", "datapoint")
-            .attr("r", "2px");
-    update_points(data);
+            .append("path")
+            .attr("class", "line")
+            .attr("d", function(d) { return d.line(d.data); });
 
     focus.append("g")
         .attr("class", "x axis")
@@ -92,42 +75,69 @@ d3.json("../../data/hubbard-avgrange-daily.json", function(error, d) {
         .attr("class", "y axis")
         .call(yAxis);
 
-    context.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line2);
+    
+};
 
-    context.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height2 + ")")
-        .call(xAxis2);
 
-    context.append("g")
-        .attr("class", "x brush")
-        .call(brush)
-        .selectAll("rect")
-        .attr("y", -6)
-        .attr("height", height2 + 7);
+timeseries.forEach(function(ts) {
+    d3.jsonp(ts.url, function(data) {
+        var yscale = d3.scale.linear()
+            .range([0, height])
+            .domain(d3.extent(data.map(dataValue)).reverse())
+            .nice();
+        var yscale2 = d3.scale.linear()
+            .range([0, height2])
+            .domain(d3.extent(data.map(dataValue)).reverse())
+            .nice();
+        var line = d3.svg.line()
+            .defined(function(d) { return d.value; })
+            .x(function(d) { return xscale(d.date_time); })
+            .y(function(d) { return yscale(d.value); });
+        var line2 = d3.svg.line()
+            .defined(function(d) { return d.value; })
+            .x(function(d) { return xscale2(d.date_time); })
+            .y(function(d) { return yscale2(d.value); });
+        var localXscale = d3.time.scale()
+            .domain(d3.extent(data.map(function(d) {
+                return parseDate(d.date_time);
+            })));
+        data = localXscale.ticks(d3.time.day, 1).reduce(function(previous, current) {
+            var d = {"date_time": current};
+            if (parseDate(data[0].date_time) <= current)
+                d.value = data.shift().value;
+            else
+                d.value = null;
+            return previous.concat(d);
+        }, []);
+        loadedData.push({
+            "name": ts.name,
+            "slug": ts.slug,
+            "data": data,
+            "color": ts.color,  // TODO allow color generation
+            "yscale": yscale,
+            "line": line,
+            "line2": line2,
+            "visible": true     // TODO allow per-ts customization
+        });
+        // TODO sort loaded data by something?
+        if (loadedData.length == timeseries.length)
+            draw();
+    });
 });
 
-function brushed() {
-    xscale.domain(brush.empty() ? x2scale.domain() : brush.extent());
-    focus.select(".line")
-        .attr("d", line);
-    focus.select(".x.axis")
-        .call(xAxis);
-    update_points(data.filter(function(d) {
-        return xscale(d.date_time) > 0;
-    }));
+
+function updateXscale() {
+    xscale.domain(d3.extent(d3.merge(visibleData().map(function(a) { return d3.extent(a.data, dataDateTime); }))));
+    x2scale.domain(xscale.domain());
 }
 
 
-function update_points(p) {
-    var points = focus.selectAll("circle.datapoint").data(p, date_time);
+function updatePoints(points) {
     points.enter()
         .append("circle")
         .attr("class", "datapoint")
-        .attr("r", "2px");
+        .attr("r", "2px")
+        .attr("r", function(d) { return d.value ? "2px": "0px"; });
 
     points.attr("cx", function(d) { return xscale(d.date_time); })
         .attr("cy", function(d) { return yscale(d.value); });
@@ -136,6 +146,16 @@ function update_points(p) {
 }
 
 
-function date_time(d) {
-    return d.date_time.toString();
+function visibleData() {
+    return loadedData.filter(function(d) { return d.visible; });
+}
+
+
+function dataValue(d) {
+    return d.value;
+}
+
+
+function dataDateTime(d) {
+    return d.date_time;
 }
